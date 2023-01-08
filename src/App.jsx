8a3da04927a46ed10104ef828002b8e27790baa3
Utsav173 <81065457+Utsav173@users.noreply.github.com>
2023-01-08 23:05:57 +0530

    n76

--- src/App.jsx
+++ src/App.jsx
@@ -40,11 +40,6 @@ function App() {
         <Route exact path="*" element={<Home mode={mode}/>}/>
         <Route exact path="/about" element={<About mode={mode}/>}/>
         <Route exact path="/download" element={<Download mode={mode}/>}/>
-        <Route exact path="/gtuimp" element={<Paper linka="gtuimp"/>}/>
-        <Route exact path="/adudpaper" element={<Paper linka="adudpaper"/>}/>
-        <Route exact path="/iswapaper" element={<Paper linka="iswapaper"/>}/>
-        <Route exact path="/varpaper" element={<Paper linka="varpaper"/>}/>
-        <Route exact path="/cvpaper" element={<Paper linka="cvpaper"/>}/>
       </Routes>
     </Router>
    
