9bcc234fd7d3db4ccb7f4bc6cc30eaf8b4d52a1e
Utsav173 <81065457+Utsav173@users.noreply.github.com>
2023-01-25 12:30:00 +0530

    jj

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,42 +1,47 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { Button, Card, Col, Container, Form, FormGroup, Row } from "react-bootstrap";
 import { Input } from "@mui/material";
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
-  const [titile, setTitile] = useState('movies');
+  const [searchTerm, setSearchTerm] = useState("loki");
 
+  let term;
   // https://www.omdbapi.com/?i=tt3896198&apikey=485c72dd
 
   const API_LINK = "https://www.omdbapi.com/?apikey=677e5307"
 
-  const searchMvies = async () => {
-    const response = await axios.get(
-      `${API_LINK}&s=${titile}`
-    );
-    const data = await response.data.Search;
-    console.log(data);
-    setMovies(data);
-    console.log(movies);
-  };
+  useEffect( () => {
+        searchMovies();
+    }, [] );
+ 
+    const searchMovies = async () => {
+        const response = await fetch(`${API_LINK}&s=${searchTerm}`);
+        const data = await response.json();
 
-  useEffect(() => {
-    searchMvies();
-  }, [titile]);
+        setMovies(data.Search);
+        console.log(data);
+    };
+
+
 
   return (
-    <Container className="m-4">
-    <Row className="my-2 justify-content-center">
-    <Input placeholder="enter movie name" name="Search title" value={titile} onChange={e=> setTitile(e.target.value)}/>
+    <Container className="d-flex flex-column justify-content-center mx-3 mt-4">
+    <Row className="my-2 d-flex flex-row justify-content-center">
+    
+      <Input placeholder="Search Movie here"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)} 
+                    />
+ <Button variant="success" className="ms-auto" onClick={() => searchMovies(searchTerm)}>Search movie</Button>
     </Row>
       <Row gap={2}>
       {
         movies.map((mv, index)=>{
           return (
             <Col>
-              <Card style={{ width: "18rem" }} key={index}>
-                <Card.Img variant="top" src={mv.Poster} />
+              <Card style={{ width: "18rem" }} key={index} className="m-2">
+                <Card.Img variant="top" src={mv.Poster} alt={mv.Title} />
                 <Card.Body>
                   <Card.Title>{mv.Title}</Card.Title>
                   <Card.Text>{mv.Year}</Card.Text>
