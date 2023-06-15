12d2b0d50f7da127878559f96d2d3da4435e223f
utsav173 <khatriutsav40@gmail.com>
2023-06-15 22:32:28 +0530

    chnage scale issue in small devices

--- src/app/paper/[id]/page.js
+++ src/app/paper/[id]/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { Container } from "@chakra-ui/react";
+import { Container, useMediaQuery } from "@chakra-ui/react";
 import { Viewer, Worker } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
@@ -8,11 +8,19 @@ import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 const PaperPage = ({ params: { id } }) => {
   const bookData = `https://utsav173.github.io/samplepaper/${id}.pdf`;
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const [isTabletOrSmaller] = useMediaQuery("(max-width: 768px)");
 
   return (
     <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.7.107/pdf.worker.min.js">
       <Container>
-        <Viewer fileUrl={bookData} />
+        <Viewer
+          fileUrl={bookData}
+          enableSmoothScroll
+          defaultScale={isTabletOrSmaller ? 0.9 : undefined}
+          style={{
+            width: isTabletOrSmaller ? `${window.innerWidth}px` : "auto",
+          }}
+        />
       </Container>
     </Worker>
   );
